b7382ecd6f09f3edef2f988aa5c0454f0e4da561
Juarez-MMMarta <martajua38@gmail.com>
2023-05-19 21:58:44 -0300

    Actualizacion de la capa model

--- TP2_Grupo12/src/main/java/ar/edu/unju/fi/listas/ListaContacto.java
+++ TP2_Grupo12/src/main/java/ar/edu/unju/fi/listas/ListaContacto.java
@@ -1,5 +1,17 @@
 package ar.edu.unju.fi.listas;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import ar.edu.unju.fi.model.Contacto;
+
 public class ListaContacto {
+
+	private List<Contacto> contactos;
+		public ListaContacto() {
+			contactos = new ArrayList<Contacto>();
+			
+			contactos.add(new Contacto());
+	}
 
 }
