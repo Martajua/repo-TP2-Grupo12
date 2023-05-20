0d6816abdfc68a1e81ec76a44b7a9a494e25ccd5
María Marta del Milagro Juarez <103362290+Martajua@users.noreply.github.com>
2023-05-20 19:25:24 -0300

    Controller sucursal (#51)
    * Commit Editando SucursalController - IndexController
    
    * Commit SucursalController, se agrego todas las peticiones.
    
    * Commit correccion de errores en SucursalController

--- TP2_Grupo12/src/main/java/ar/edu/unju/fi/controller/SucursalController.java
+++ TP2_Grupo12/src/main/java/ar/edu/unju/fi/controller/SucursalController.java
@@ -3,7 +3,16 @@ package ar.edu.unju.fi.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import ar.edu.unju.fi.listas.ListaSucursal;
+import ar.edu.unju.fi.model.Sucursal;
+
+
 
 @Controller
 @RequestMapping("/sucursal")
