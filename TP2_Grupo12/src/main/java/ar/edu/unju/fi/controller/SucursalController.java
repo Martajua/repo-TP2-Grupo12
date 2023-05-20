9f9c49cdbe36875aa7ba817a0c9e1f33cec5ed6a
María Marta del Milagro Juarez <103362290+Martajua@users.noreply.github.com>
2023-05-20 19:21:06 -0300

    Controller sucursal (#50)
    * Commit Editando SucursalController - IndexController
    
    * Commit SucursalController, se agrego todas las peticiones.

--- TP2_Grupo12/src/main/java/ar/edu/unju/fi/controller/SucursalController.java
+++ TP2_Grupo12/src/main/java/ar/edu/unju/fi/controller/SucursalController.java
@@ -1,16 +1,84 @@
 package ar.edu.unju.fi.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/sucursal")
 public class SucursalController {
+	
+	ListaSucursal listaSucursales = new ListaSucursal();
 
 	@GetMapping("/listado")
 	public String getSucursal() {
+		return "sucursales";
+	}
+	
+	@GetMapping("/listado")
+	public String getListaSucursalPage(Model model) {
+		model.addAttribute("sucursales", listaSucursales.getSucursales());
 		
 		return "sucursales";
 	}
+	
+	@GetMapping("/nuevo")
+	public String getNuevaSucursalPage(Model model) {
+		boolean edicion=false;
+		model.addAttribute("sucursal", new Sucursal());
+		model.addAttribute("edicion", edicion);
+		return "nueva_sucursal";
+	}
+	
+	@PostMapping("/guardar")
+	public ModelAndView getGuardarSucursalPage(@ModelAttribute("sucursal")Sucursal sucursal) {
+		ModelAndView modelView = new ModelAndView("sucursales");
+		listaSucursales.getSucursales().add(sucursal);
+		modelView.addObject("sucursales", listaSucursales.getSucursales());
+		return modelView;
+	}
+	
+	@GetMapping("/modificar/{id}")
+	public String getModificarSucursalPage(Model model, @PathVariable(value="id")String id) {
+		Sucursal sucursalEncontrada = new Sucursal();
+		boolean edicion=true;
+		for(Sucursal sucu : listaSucursales.getSucursales()) {
+			if(sucu.getId().equals(id)) {
+				sucursalEncontrada = sucu;
+				break;
+			}
+		}
+		model.addAttribute("sucursal", sucursalEncontrada);
+		model.addAttribute("edicion", edicion);
+		return "formSucursal";
+	}
+	
+	@PostMapping("/modificar")
+	public String modificarSucursal(@ModelAttribute("sucursal")Sucursal sucursal){
+		for(Sucursal sucu: listaSucursales.getSucursales()) {
+			if(sucu.getId().equals(sucursal.getId())) {
+				sucu.setId(sucursal.getId());
+				sucu.setDireccion(sucursal.getDireccion());
+				sucu.setHorario(sucursal.getHorario());
+				sucu.setNombre(sucursal.getNombre());
+				sucu.setProvincia(sucursal.getProvincia());
+				sucu.setTelefono(sucursal.getTelefono());
+			}
+		}
+		return "redirect:/sucursal/listado";
+	}
+	
+	@GetMapping("/eliminar/{id}")
+	public String getEliminatSucursalPage(Model model, @PathVariable(value="id")String id) {
+		
+		for(Sucursal sucu: listaSucursales.getSucursales()) {
+			if(sucu.getNombre().equals(id)) {
+				listaSucursales.getSucursales().remove(sucu);
+				break;
+			}
+		}
+		return "redirect:/sucursal/listado";
+	}
 }
+
