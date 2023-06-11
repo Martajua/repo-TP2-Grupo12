cad9edeecc9d1d215a41c55ad03300b633bc126c
Juarez-MMMarta <martajua38@gmail.com>
2023-06-11 10:19:41 -0300

    En la esctructura de la capa Service modifique consejo

--- TP2_Grupo12/src/main/java/ar/edu/unju/fi/service/IConsejoService.java
+++ TP2_Grupo12/src/main/java/ar/edu/unju/fi/service/IConsejoService.java
@@ -1,5 +1,21 @@
 package ar.edu.unju.fi.service;
 
+import java.util.List;
+
+import ar.edu.unju.fi.model.Consejo;
+import jakarta.validation.Valid;
+
 public interface IConsejoService {
 
+	List<Consejo> getListaConsejo();
+	
+	void guardar(@Valid Consejo consejo);
+	
+	Consejo buscar(int num);
+	
+	void modificar(Consejo consejo);
+	
+	void eliminar(Consejo consejoEncontrado);
+	
+	Consejo getConsejo();
 }
--- TP2_Grupo12/src/main/java/ar/edu/unju/fi/service/imp/ConsejoServiceImp.java
+++ TP2_Grupo12/src/main/java/ar/edu/unju/fi/service/imp/ConsejoServiceImp.java
@@ -1,7 +1,71 @@
 package ar.edu.unju.fi.service.imp;
 
-import ar.edu.unju.fi.service.IConsejoService;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import ar.edu.unju.fi.listas.ListaConsejo;
+import ar.edu.unju.fi.model.Consejo;
+import ar.edu.unju.fi.service.IConsejoService;
+import jakarta.validation.Valid;
+
+@Service
 public class ConsejoServiceImp implements IConsejoService {
+	
+	/*
+	 * Lista de consejos
+	 */
+	@Autowired
+	private ListaConsejo listaConsejos;
+	/*
+	 * Nuevo consejo
+	 */
+	@Autowired
+	private Consejo consejo;
+
+	@Override
+	public List<Consejo> getListaConsejo() {
+		return listaConsejos.getConsejos();
+	}
+	
+	@Override
+	public void guardar(@Valid Consejo consejo) {
+		listaConsejos.getConsejos().add(consejo);
+	}
+	
+	@Override
+	public Consejo buscar(int num) {
+		Consejo consejoEncontrado = null;
+		for(Consejo consj: listaConsejos.getConsejos()) {
+			if(consj.getNum()==num) {
+				consejoEncontrado = consj;
+				break;
+			}
+		}
+		return consejoEncontrado;
+	}
+
+	@Override
+	public void modificar(Consejo consejo) {
+		for(Consejo consj: listaConsejos.getConsejos()) {
+			if(consj.getNum() == consejo.getNum()) {
+				consj.setTitulo(consj.getTitulo());
+				consj.setMensaje(consj.getMensaje());
+			}
+		}
+	}
+
+	@Override
+	public void eliminar(Consejo consejoEncontrado) {
+		listaConsejos.getConsejos().remove(consejoEncontrado);
+		
+	}
+
+	@Override
+	public Consejo getConsejo() {
+		return consejo;
+	}
+	
 
 }
