f0adec2237abfa96420f4cc8fb37c4704808b43d
Federico Salinas <fedeasalinas@gmail.com>
2023-06-11 21:07:11 -0300

    se aplicaron servicios en los controladores "Service" y "Producto".

--- TP2_Grupo12/src/main/java/ar/edu/unju/fi/service/IProductoService.java
+++ TP2_Grupo12/src/main/java/ar/edu/unju/fi/service/IProductoService.java
@@ -1,5 +1,23 @@
 package ar.edu.unju.fi.service;
 
+import java.util.List;
+
+import ar.edu.unju.fi.model.Producto;
+
 public interface IProductoService {
 
+	
+	
+	List<Producto> getLista();
+	
+	void guardar(Producto producto);
+
+	Producto buscar(String id);
+
+	void modificar(Producto producto);
+
+	void eliminar(Producto productoEncontrado);
+
+	Producto getProducto();
+	
 }
--- TP2_Grupo12/src/main/java/ar/edu/unju/fi/service/imp/ProductoServiceImp.java
+++ TP2_Grupo12/src/main/java/ar/edu/unju/fi/service/imp/ProductoServiceImp.java
@@ -1,7 +1,64 @@
 package ar.edu.unju.fi.service.imp;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import ar.edu.unju.fi.listas.ListaProducto;
+import ar.edu.unju.fi.model.Producto;
 import ar.edu.unju.fi.service.IProductoService;
 
+@Service
 public class ProductoServiceImp implements IProductoService {
+
+	@Autowired
+	private ListaProducto listaProducto;
+	@Autowired
+	private Producto producto;
+
+	public List<Producto> getLista() {
+		return listaProducto.getProductos();
+	}
+
+	public void guardar(Producto producto) {
+		listaProducto.getProductos().add(producto);
+		producto.calcularDescuento();
+	}
+
+	public Producto buscar(String id) {
+		Producto productoEncontrado = null;
+		for (Producto prod : listaProducto.getProductos()) {
+			if (prod.getCodigo().equals(id)) {
+				productoEncontrado = prod;
+				break;
+			}
+		}
+		return productoEncontrado;
+	}
+
+	public void modificar(Producto producto) {
+		for (Producto prod : listaProducto.getProductos()) {
+			if (prod.getCodigo().equals(producto.getCodigo())) {
+				prod.setCategoria(producto.getCategoria());
+				prod.setCodigo(producto.getCodigo());
+				prod.setNombre(producto.getNombre());
+				prod.setDescuento(producto.getDescuento());
+				prod.setPrecio(producto.getPrecio());
+				prod.setPrecioFinal(producto.calcularDescuento());
+			}
+
+		}
+
+	}
+
+	public void eliminar(Producto productoEncontrado) {
+		listaProducto.getProductos().remove(productoEncontrado);
+
+	}
+
+	public Producto getProducto() {
+		return producto;
+	}
 
 }
